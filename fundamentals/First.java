775f708e03aa4f81e7a31ce7b25f70fdbf21085d
StevenM510 <mooresteven510@gmail.com>
2021-09-27 11:25:06 -0700

    first commit

---
+++ fundamentals/First.java
@@ -0,0 +1,14 @@
+public class First {
+
+    public static void main(String[] args){
+
+        String name = " Steven Moore ";
+        int age = 30;
+        String hometown = " Oakland, Ca ";
+
+        System.out.println(" My name is " + name);
+        System.out.println(" I am " + age + " years old ");
+        System.out.println(" My Hometown is " + hometown);
+    }
+}
+
